845065f54dff617fbd3ac5796eb28880c4e1f28f
ushmaa <ushmatalreja@gmail.com>
2025-03-22 23:23:34 +0530

    created the game

---
+++ guessing_game.c
@@ -0,0 +1,39 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <time.h>
+
+ 
+ int main()
+ {
+    int random, guess;
+    int no_of_guess = 0;
+    srand(time(NULL));
+
+    printf("Welcome to the guessing number game\n");
+    random = rand() * 100 +1;
+
+    do{
+        printf("\nPlease enter your guess between (1 to 100):");
+        scanf(%d,&guess);
+        no_of_guess++;
+
+        if (guess < random)
+        {
+            printf("Guess a larger number.\n");
+        }
+        else if(guess > random)
+        {
+            printf("Guess a smaller number.\n");
+        }
+        else
+        {
+            printf("Congratulations !!!You have successfully guessed the number in %d attempt", no_of_guess);
+        }       
+     }
+    
+    while (guess != random);
+    {
+        printf("\nBye Bye Thnaks for Playing.");
+        printf("\n Developed By : Ushma Talreja");
+    }
+}

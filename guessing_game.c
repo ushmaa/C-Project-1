4cbf3e96738b720c8f930ac406e9f4d9c700e517
ushmaa <ushmatalreja@gmail.com>
2025-03-24 02:19:23 +0530

    created html file for output

--- guessing_game.c
+++ guessing_game.c
@@ -2,6 +2,31 @@
  #include <stdlib.h>
  #include <time.h>
 
+ void writeToHTML(int no_of_guess, int random, int guess)
+{
+    FILE *fp = fopen("output.html", "w");
+    fprintf(fp, "<!DOCTYPE html>\n<html>\n<head>\n");
+    fprintf(fp, "<link rel='stylesheet' type='text/css' href='style.css'>\n");
+    fprintf(fp, "<title>Guessing Game Result</title>\n</head>\n<body>\n");
+
+    if (guess == random)
+    {
+        fprintf(fp, "<div class='result success'>");
+        fprintf(fp, "<h1>🎉 Congratulations!</h1>\n");
+        fprintf(fp, "<p>You guessed the number in %d attempts!</p>\n", no_of_guess);
+    }
+    else
+    {
+        fprintf(fp, "<div class='result failure'>");
+        fprintf(fp, "<h1>❌ Game Over!</h1>\n");
+        fprintf(fp, "<p>Oops! Better luck next time.</p>\n");
+    }
+
+    fprintf(fp, "</div>\n</body>\n</html>");
+    fclose(fp);
+}
+
+
  
  int main()
  {
